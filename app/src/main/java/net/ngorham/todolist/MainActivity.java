162fa9fd95af06c230bd1170a01902865f3c9437
ngorham <ngorham2@gmail.com>
2018-03-08 11:35:13 -0800

    ToDoList Update 2 3/08/2018
    Added Action bar with clickable items

--- app/src/main/java/net/ngorham/todolist/MainActivity.java
+++ app/src/main/java/net/ngorham/todolist/MainActivity.java
@@ -2,6 +2,8 @@ package net.ngorham.todolist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,29 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Displays Action Bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        //Inflate menu, add items to action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Call when user clicks an item in action bar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        //Handle action items
+        switch(item.getItemId()){
+            case R.id.add_list:
+                //Add list action
+                return true;
+            case R.id.app_settings:
+                //Settings action
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
